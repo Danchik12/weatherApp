9a248d60960c574bb88120d464dbb61c7db310cb
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-15 20:07:46 +0300

    create API redux

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './styles/index.scss'
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import ThemeProvider from './Provider/ThemeProvider'
+import {store} from './store/store';
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
    <BrowserRouter>
    <ThemeProvider>
     <App />
     </ThemeProvider>
      </BrowserRouter>
+     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,10 @@
-import (configureStore) from '@reduxjs/toolkit'
+import {configureStore,combineReducers} from '@reduxjs/toolkit'
 
-export const store = configureStore()
+const rootReducer = combineReducers({})
+
+export const store = configureStore({rootReducer,})
+
+
+export type RootState = typeof rootReducer;
+export type AppStore= typeof store;
+export type AppDispatch = AppStore['dispatch'];
