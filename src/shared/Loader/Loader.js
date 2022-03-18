fffe747a5ebac96e81fb02db85ebfa7ba07520f1
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-03-18 11:51:38 +0300

    loader in center

--- src/shared/Loader/Loader.js
+++ src/shared/Loader/Loader.js
@@ -4,7 +4,8 @@ import s from './Loader.module.scss'
 
 export default function Loader() {
 	return (
-	
+	<div className={s.container}>
 		<div className={s.lds_ring}><div></div><div></div><div></div><div></div></div>
+		</div>
 		)
 }
